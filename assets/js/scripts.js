a8b85957abdaa1c44525cb8f908a8220dfc51d02
Mahmudul Hasan Shafin <mhshafin220@gmail.com>
2022-02-05 23:24:55 +0600

    carousel added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
     $('.preloader').delay(500).fadeOut('slow');
     $('body').delay(300).css({'overflow':'visible'});
+    $('#test-slider').owlCarousel({
+        loop: true,
+        autoplay: true,
+        items: 1
+
+    })
+    $('#product-card-wrapper').owlCarousel({
+        loop: true,
+        items: 3,
+        center:true,
+        navs:true,
+        navText:['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>']
+    })
+    
+    
 });
